3c3f2a7e6afbc01dac58a380933b338b439d1e9f
IsraelMartinez15 <israelmtznaruto@gmail.com>
2022-10-28 16:25:24 -0500

    calculadora con codigo java y se modificaron los demas archivos

--- EV3/js4/js5.js
+++ EV3/js4/js5.js
@@ -21,3 +21,44 @@ function tabla(){
         }
     }
 }
+
+/* CALCULADORA */
+
+operaciones =["+","-","*","/"];
+
+for(var i=0; i<operaciones.length; i++){
+    var select = document.getElementById("operacion");
+    var option = document.createElement("OPTION");
+    select.options.add(option);
+    option.text = operaciones[i];
+    option.value = operaciones[i];
+}
+
+function calculadora() {
+    let numero1 = document.getElementById("numero1").value;
+    let numero2 = document.getElementById("numero2").value;
+    let operacion = document.getElementById("operacion").value;
+
+    parseInt(numero1);
+    parseInt(numero2);
+
+
+    let mensaje4 = document.getElementById("mensaje4");
+
+
+    if(numero1.length == 0 || numero2.length == 0){
+        mensaje4.innerText = `Ingresa los datos <br>`;    
+    }
+    else{
+        if(isNaN(numero1) || isNaN(numero2)) {
+            mensaje4.innerHTML = `Solo numeros <br>`;
+        }
+        else {
+            resultado = eval(numero1 + operacion + numero2);
+            mensaje4.innerHTML = `Resultado: ${resultado}<br>`;
+        }
+        
+    }
+
+
+}
